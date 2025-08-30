755a9ecc3c53e57081d7493f261ee0b7bd2bca86
Hermeson Alves <hermesonalves256@gmail.com>
2025-08-30 14:26:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+     <div className="flex min-h-svh flex-col items-center justify-center">
+      Deu o Carai
+    </div>
+
+  )
+}
+
+export default App
